22247d365c2a71cb903258f8488c4c8464d40814
zane-programs <zstjohn22@windwardschool.org>
2020-12-10 19:11:10 -0800

    minor changes to config

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,17 @@
 const path = require("path");
 
 module.exports = {
-  paths: function(paths, env) {
+  paths: function (paths, _env) {
     paths.appIndexJs = path.resolve(__dirname, "client/index.tsx");
     paths.appSrc = path.resolve(__dirname, "client");
-    paths.appTypeDeclarations = path.resolve(__dirname, "client/react-app-env.d.ts");
+    paths.appTypeDeclarations = path.resolve(
+      __dirname,
+      "client/react-app-env.d.ts"
+    );
+    paths.testsSetup = path.resolve(__dirname, "client/setupTests");
+    // paths.proxySetup = path.resolve(__dirname, "client/setupProxy.ts");
+    // paths.swSrc = path.resolve(__dirname, "client/service-worker");
+
     return paths;
   },
 };
