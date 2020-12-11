8c4b5b7cab5d8d8f099557991d9ea8b595ad6b8c
zane-programs <zstjohn22@windwardschool.org>
2020-12-10 20:35:13 -0800

    init server code (still more work to be done)

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 const express = require("express");
+
+// grab environment variables from .env
+require("dotenv").config();
+
+const app = express();
+
+app.get("/", (_req, res) => res.status(200).send("Hello world!"));
+
+const listener = app.listen(process.env.API_PORT, () =>
+  console.log(`Server listening on port ${listener.address().port}`)
+);
