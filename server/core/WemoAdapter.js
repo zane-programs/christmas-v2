33c3cd10257550575bdac2a60ee0eb2084722da3
zane-programs <zstjohn22@windwardschool.org>
2020-12-19 15:30:27 -0800

    another failsafe added, may remove later

--- server/core/WemoAdapter.js
+++ server/core/WemoAdapter.js
@@ -6,6 +6,17 @@ module.exports = class WemoAdapter {
     this._changeListeners = [];
     this._wemo = new Wemo();
     this._serialNumber = serialNumber;
+    this._previousState = null;
+
+    // workaround part 2
+    setInterval(async () => {
+      // just in case the subscription fails
+      let currentState = await this.getState();
+      if (this._previousState != currentState) {
+        this._runChangeListeners(currentState);
+      }
+      this._previousState = currentState;
+    }, 1000);
   }
 
   init(cb) {
@@ -17,7 +28,7 @@ module.exports = class WemoAdapter {
         console.log("found THE one");
         self._client = self._wemo.client(deviceInfo);
         self._client.on("binaryState", (value) => {
-          console.log(`Binary state changed to ${value}`);
+          console.log(`FROM SUBSCRIPTION - Binary state changed to ${value}`);
           self._runChangeListeners(value);
         });
         cb(); // done!
