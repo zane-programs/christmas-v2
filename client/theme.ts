c5b32e482f7dc72fbdd3e1b88e386ac9960aa81b
Zane St. John <zstjohn22@windwardschool.org>
2020-12-25 02:38:46 -0800

    test webhook 2

--- client/theme.ts
+++ client/theme.ts
@@ -11,7 +11,8 @@ export interface Theme {
 }
 
 const theme: Theme = {
-  mainColor: "#b3000c",
+  // mainColor: "#b3000c",
+  mainColor: "#00ff00",
   backgroundColor: "#ffe9ea",
   confettiColors: ["#b3000c", "#42692f"],
   topBarHeight: "75px",
